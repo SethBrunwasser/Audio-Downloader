dee8ed655d69c8f46cebb68d2dbbaf930b095be7
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-12-10 16:08:43 -0700

    query()
    Functiont o query youtube search which returns a list of the urls of each video.

--- youtube.py
+++ youtube.py
@@ -1,4 +1,23 @@
 import pafy
+import urllib.parse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def query(searchString):
+	'''
+	Params: searchString - To query youtube
+	Return: List of urls to videos in response to the search query 
+	'''
+	query = urllib.parse.quote(searchString)
+	url = "https://www.youtube.com/results?search_query=" + query
+	response = urlopen(url)
+	html = response.read()
+	soup = BeautifulSoup(html)
+
+	urls = []
+	for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+		urls.append('https://www.youtube.com' + vid['href'])
+	return urls
 
 def get_audio(url):
 	video = pafy.new(url)
@@ -8,4 +27,5 @@ def get_audio(url):
 
 	bestaudio.download()
 
-get_audio("https://www.youtube.com/watch?v=uUh2IaBW-zQ")
+
+print(query("DNMO Sub Urban - Broken (COFRESI Remix)"))
